708ef163bc57f3261363e5c1adf54761bd617984
Miguel Alcala <miguelalc099@gmail.com>
2026-05-22 16:29:40 -0700

    Primer commit

---
+++ backend/models/Lavado.js
@@ -0,0 +1,88 @@
+const mongoose =
+require("mongoose");
+
+const lavadoSchema =
+new mongoose.Schema({
+
+folio: {
+  type: String
+},
+
+fechaLavado: {
+  type: Date
+},
+
+numeroUnidad: {
+  type: String
+},
+
+tipoUnidad: {
+  type: String
+},
+
+operadores: [{
+  type: String
+}],
+
+cantidadOperadores: {
+  type: Number
+},
+
+tiposLavado: [{
+  type: String
+}],
+
+comentarios: {
+  type: String
+},
+
+fotosAntes: [{
+  type: String
+}],
+
+fotosDespues: [{
+  type: String
+}],
+
+estatus: {
+
+  type: String,
+
+  default:
+    "EN_ESPERA"
+},
+
+comentarioSupervisor: {
+  type: String
+},
+
+firmaSupervisor: {
+  type: String
+},
+
+aprobadoPor: {
+  type: String
+},
+
+fechaAprobacion: {
+  type: Date
+},
+
+bloqueado: {
+
+  type: Boolean,
+
+  default: false
+}
+
+}, {
+
+timestamps: true
+
+});
+
+module.exports =
+mongoose.model(
+  "Lavado",
+  lavadoSchema
+);
